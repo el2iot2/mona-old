8b9164298a41809c85752a9b5da1941c8474b08b
Elliott B. Edwards <elliott.b.edwards@gmail.com>
2014-06-24 16:00:21 -0700

    Added Test for Beard Templates

---
+++ src/Mona.Test/Examples/BeardTemplateShould.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Mona.Test.Examples
+{
+    /// <summary>
+    /// Mini language for http://github.com/automatonic/beard
+    /// </summary>
+    public class BeardTemplateShould
+    {
+        //[Fact]
+        //void ComposeBeardTemplateParser()
+        //{
+        //    var FrontMatterDefinitions = Parsers.Create<char, char>(null);
+        //    var FrontMatter = Parsers.Concat(
+        //        Parsers.Chars("<!--"),
+        //        Parsers.While<
+        //        Parsers.Chars("-->")
+        //        );
+        //}
+    }
+}
