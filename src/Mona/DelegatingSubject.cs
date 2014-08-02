d2c33760be9eb0cebd6139374b2a6520a71c7790
Elliott B. Edwards <elliott.b.edwards@gmail.com>
2014-08-02 01:15:30 -0700

    Merge remote-tracking branch 'origin/beard' into beard
    Conflicts:
    	src/Mona.Test/Expect/CharNoParamShould.cs
    	src/Mona.Test/Expect/CharPredicateShould.cs
    	src/Mona.Test/Expect/CharShould.cs
    	src/Mona/Expect.Sequence.cs
    	src/Mona/Expect.Single.cs
    	src/Mona/Expect.While.cs
    	src/Mona/Parser.cs

---
+++ src/Mona/DelegatingSubject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mona
+{
+    /// <summary>
+    /// A subject that delegates ownership of its observable sequence to 
+    /// </summary>
+    class DelegatingSubject
+    {
+    }
+}
