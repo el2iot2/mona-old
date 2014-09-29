3c5f82c221ff2516d3bb69a8d7e86b611f436cfe
Elliott B. Edwards <elliott@automatonic.net>
2014-09-28 19:08:24 -0700

    Fixes #15
    Removes beard example

---
+++ src/Mona.Test/Expect/ParserShould.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Mona
+{
+    public partial class ParserShould
+    {
+        [Fact]
+        public void SelectNodeWhenSucceeded()
+        {
+            var parser = Expect
+                .Char()
+                .SelectNode(input => input.ToString());
+
+            var parse = parser.Parse("a");
+            parse.Succeeded().Should().BeTrue();
+            parse.Node.Should().Be("a");
+            parse.Remainder.Should().BeEmpty("the entire input should be consumed");
+        }
+
+        [Fact]
+        public void SelectNodeWhenFailed()
+        {
+            var parser = Expect
+                .Char(Char.IsDigit)
+                .SelectNode(input => input.ToString());
+
+            var parse = parser.Parse("a");
+            parse.Failed().Should().BeTrue();
+            parse.Node.Should().Be(default(char).ToString());
+            parse.Remainder.Should().Equal('a');
+        }
+    }
+}
